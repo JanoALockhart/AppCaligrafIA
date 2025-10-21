7e595029cde6e2751df17b48627170920d498902
Jano Axel Lockhart <janolockhart@gmail.com>
2025-10-21 02:47:42 +0000

    Created Splitter

--- src/main.py
+++ src/main.py
@@ -1,23 +1,24 @@
-import keras
 import argparse
-
+from line_splitter import LineSplitter
 import settings
 
 def main():
     parser = argparse.ArgumentParser(description="Application to take pictures of handwritten letters (A..Z) from students and recommend which ones to practice more")
     parser.add_argument("--model", required=True, type=str, help=f"The file name of the .keras model saved in {settings.PRODUCTION_MODEL_FOLDER}")
-    
+    parser.add_argument("--file", required=True, type=str, help=f"The file name of the image of the student letters saved in {settings.PRODUCTION_MODEL_FOLDER}")
     args = parser.parse_args()
     # load image
+
+    img_path = f"{settings.PRODUCTION_MODEL_FOLDER}{args.file}"
+    letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    splitter = LineSplitter(img_path, letters)
+
     # count letters 
 
     # create model
-    model_path = f"{settings.PRODUCTION_MODEL_FOLDER}{args.model}"
-    print(model_path)
-    model = keras.models.load_model(
-        filepath=model_path,
-        compile=False
-    )
+    #model_processing(args)
+    
+    
     # load weights
     # evaluate
 
@@ -25,6 +26,12 @@ def main():
     # recommend letters
 
     pass
+
+def model_processing(args):
+    import keras
+    model_path = f"{settings.PRODUCTION_MODEL_FOLDER}{args.model}"
+    print(model_path)
+    model = keras.models.load_model(filepath=model_path, compile=False)
 
 if __name__ == "__main__":
     main()
