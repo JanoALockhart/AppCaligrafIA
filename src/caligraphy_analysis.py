4d451dc7f646c2ef505905ab0cd2f6d847f67406
Jano Axel Lockhart <janolockhart@gmail.com>
2025-11-17 23:31:08 +0000

    Clean main

--- src/caligraphy_analysis.py
+++ src/caligraphy_analysis.py
@@ -4,8 +4,22 @@ import keras
 from matplotlib import pyplot as plt
 import tensorflow as tf
 
-from main import decoding_function
+import line_splitter
+import settings
 
+
+def analyze_caligraphy(args):
+    VOCABULARY_LIST = list(" 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
+    #VOCABULARY_LIST = [' ', '!', '"', '#', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '?', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', ']', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '}', '¤', '°', '²', 'È', 'É', 'à', 'â', 'ç', 'è', 'é', 'ê', 'ë', 'î', 'ï', 'ô', 'ö', 'ù', 'û', '€']
+    print(VOCABULARY_LIST)
+    LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    decoding_function = keras.layers.StringLookup(vocabulary=VOCABULARY_LIST, oov_token="", invert=True)
+    img_path = f"{settings.IMAGES_FOLDER}{args.file}"
+    model_path = f"{settings.PRODUCTION_MODEL_FOLDER}{args.model}"
+
+    line_images = line_splitter._process_image(img_path, LETTERS)
+    recomendations = model_processing(model_path, line_images, decoding_function)
+    return recomendations
 
 def preprocess_image(img):
     start = 84
@@ -24,7 +38,7 @@ def preprocess_image(img):
     return img
 
 
-def decode_logits(logits):
+def decode_logits(logits, decoding_function):
     print(logits.shape)
 
     input_len = tf.ones(logits.shape[0]) * logits.shape[1]
@@ -34,7 +48,7 @@ def decode_logits(logits):
     return pred_string
 
 
-def model_processing(model_path, lines_images):
+def model_processing(model_path, lines_images, decoding_function):
     model = keras.models.load_model(filepath=model_path, compile=False)
 
     recomendations = {}
@@ -43,7 +57,7 @@ def model_processing(model_path, lines_images):
         img_pre = preprocess_image(lines_images[letter_line])
         img = tf.expand_dims(img_pre, axis=0)
         logits = model.predict(img)
-        predicted_string = decode_logits(logits)
+        predicted_string = decode_logits(logits, decoding_function)
 
         print("PRED", predicted_string)
 
@@ -58,3 +72,5 @@ def model_processing(model_path, lines_images):
 
     sorted_recomendations = sorted(recomendations.items(), key=lambda item: item[1])
     return sorted_recomendations
+
+
--- src/main.py
+++ src/main.py
@@ -1,14 +1,6 @@
 import argparse
-import keras
-import line_splitter
-from caligraphy_analysis import model_processing
+from caligraphy_analysis import analyze_caligraphy
 import settings
-
-#VOCABULARY_LIST = [' ', '!', '"', '#', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '?', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', ']', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '}', '¤', '°', '²', 'È', 'É', 'à', 'â', 'ç', 'è', 'é', 'ê', 'ë', 'î', 'ï', 'ô', 'ö', 'ù', 'û', '€']
-VOCABULARY_LIST = list(" 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
-
-LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-decoding_function = keras.layers.StringLookup(vocabulary=VOCABULARY_LIST, oov_token="", invert=True)
 
 def main():
     parser = argparse.ArgumentParser(description="Application to take pictures of handwritten letters (A..Z) from students and recommend which ones to practice more")
@@ -16,15 +8,9 @@ def main():
     parser.add_argument("--file", required=True, type=str, help=f"The file name of the image of the student letters saved in {settings.IMAGES_FOLDER}")
     args = parser.parse_args()
 
-    img_path = f"{settings.IMAGES_FOLDER}{args.file}"
-    
-    line_images = line_splitter._process_image(img_path, LETTERS)
-
-    # create model
-    model_path = f"{settings.PRODUCTION_MODEL_FOLDER}{args.model}"
-    print(VOCABULARY_LIST)
-    recomendations = model_processing(model_path, line_images)
+    recomendations = analyze_caligraphy(args)
     print("RECOMENDATIONS: ", recomendations)
+
 
 if __name__ == "__main__":
     main()
