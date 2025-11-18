557ec3cc3d092819562b7bd902ba4664e9cdab7d
Jano Axel Lockhart <janolockhart@gmail.com>
2025-11-18 00:35:20 +0000

    Post route

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html", models=["modelA", "modelB"])
+
+@app.route("/analyse", methods=["POST"])
+def analyse():
+    return "Making Analysis"
