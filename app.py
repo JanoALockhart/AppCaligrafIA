e62f98dc27ac410c3a50fbc95e31e104eebac618
Jano Axel Lockhart <janolockhart@gmail.com>
2025-11-18 01:21:07 +0000

    Added image preview

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import cv2
 from flask import Flask, render_template, request
 import numpy as np
+import json
 
 app = Flask(__name__)
 
+def load_manifest():
+    with open("MANIFEST.json", "r") as f:
+        return json.load(f)["models"]
+
+MODELS_LIST = load_manifest()
+
 @app.route("/")
 def home():
-    return render_template("index.html", models=["modelA", "modelB"])
+    return render_template("index.html", models=MODELS_LIST)
 
 @app.route("/analyse", methods=["POST"])
 def analyse():
